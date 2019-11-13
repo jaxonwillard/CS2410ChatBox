18b26f1c407f9b478182b0b970bd97efea755501
Jaxon Willard <jaxon.willard@aggiemail.usu.edu>
2019-11-13 13:01:24 -0700

    currently works between server and client, using server's console and client's gui

--- src/ChatBotServer.java
+++ src/ChatBotServer.java
@@ -14,27 +14,26 @@ public class ChatBotServer {
         sck = sv.accept();
         rd = new BufferedReader(new InputStreamReader(sck.getInputStream()));
         wr = new BufferedWriter(new OutputStreamWriter(sck.getOutputStream()));
-        System.out.println("hello");
 
-//        while (true) {
-//            response = rd.readLine().trim();
-//            System.out.println("DEBUG: From Client: " + response);
-//            wr.write(response + "\r\n");
-//            wr.flush();
-//        }
         while (true) {
-            System.out.println("hi");
-            try{
-                System.out.println("Tried");
-                socket = sv.accept();
-                System.out.println("again");
-            } catch (IOException e){
-                System.out.println("caught");
-                e.printStackTrace();
-            }
-            System.out.println("Debug in chatbotserver");
-            new ClientThread(socket).start();
+            response = rd.readLine().trim();
+            System.out.println("DEBUG: From Client: " + response);
+            wr.write(response + "\r\n");
+            wr.flush();
         }
+//        while (true) {
+//            System.out.println("hi");
+//            try{
+//                System.out.println("Tried");
+//                socket = sv.accept();
+//                System.out.println("again");
+//            } catch (IOException e){
+//                System.out.println("caught");
+//                e.printStackTrace();
+//            }
+//            System.out.println("Debug in chatbotserver");
+//            new ClientThread(socket).start();
+//        }
 
     }
 }
--- src/ClientThread.java
+++ src/ClientThread.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ClientThread extends Thread {
